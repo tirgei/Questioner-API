c2673f0d7fa003865435804a9d310db159d50504
Vincent Tirgei <tirgeic@gmail.com>
2019-01-09 00:27:10 +0300

    [Feature #163040277] Updated password validator

--- app/api/v1/utils/validator.py
+++ app/api/v1/utils/validator.py
@@ -21,6 +21,30 @@ def email(value):
 
 def password(password):
     """ Validate password is Strong """
-    if not re.match(r'[A-Za-z0-9@#$%^&+=]{6,}', password):
+
+    flag = 0
+    while True:   
+        if (len(password)<8): 
+            flag = -1
+            break
+        elif not re.search("[a-z]", password): 
+            flag = -1
+            break
+        elif not re.search("[A-Z]", password): 
+            flag = -1
+            break
+        elif not re.search("[0-9]", password): 
+            flag = -1
+            break
+        elif not re.search("[_@$#&^%]", password): 
+            flag = -1
+            break
+        elif re.search("\s", password): 
+            flag = -1
+            break
+        else: 
+            break
+    
+    if flag == -1:
         raise ValidationError('Weak password provided')
         
