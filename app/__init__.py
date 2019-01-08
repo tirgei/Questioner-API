e853028d98810dc07434e01718d15ae31e0e3cf7
Vincent Tirgei <tirgeic@gmail.com>
2019-01-08 22:22:24 +0300

    [Feature #163040277] Initialized JWT Manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from instance.config import app_config
+from flask_jwt_extended import (JWTManager)
 
 def create_app(config_name):
     """ Function to initialize Flask app """
@@ -8,5 +9,8 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+
+    # Initialize JWT
+    jwt = JWTManager(app)
 
     return app
