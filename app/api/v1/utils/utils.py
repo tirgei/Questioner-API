8ac747794951e2e9e643a823e7ca9a64562e12b8
Vincent Tirgei <tirgeic@gmail.com>
2019-01-08 22:37:21 +0300

    [Feature #163040277] Added generate id function

---
+++ app/api/v1/utils/utils.py
@@ -0,0 +1,10 @@
+
+@staticmethod
+def generate_id(collection):
+    """ Function to generate ID for collection """
+
+    # If collection is empty return 1 else add 1 to id of last object
+    if len(collection) == 0:
+        return 1
+    else:
+        return collection[-1]['id']+1
