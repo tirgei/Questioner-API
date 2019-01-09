00c7d446dd0514cbe47d864df57d4e3a8bfcc6da
Vincent Tirgei <tirgeic@gmail.com>
2019-01-09 10:53:42 +0300

    [Start #163039743] Added tests to fetch specific meetup

--- app/tests/v1/test_meetups.py
+++ app/tests/v1/test_meetups.py
@@ -60,3 +60,37 @@ class TestMeetups(BaseTest):
         self.assertEqual(res.status_code, 201)
         self.assertEqual(data['status'], 201)
         self.assertEqual(data['message'], 'Meetup created successfully')
+
+    def test_fetch_specific_meetup(self):
+        """ Test fetch a specific meetup using id """
+        # Create meetups
+        meetup = {
+            'topic' : 'Leveling up with Python',
+            'location' : 'Andela HQ, Nairobi',
+            'happening_on' : '08/01/2019',
+            'tags' : ['python', 'flask']
+        }
+        meetup2 = {
+            'topic' : 'Leveling up with Python',
+            'location' : 'Andela HQ, Nairobi',
+            'happening_on' : '08/01/2019',
+            'tags' : ['python', 'flask']
+        }
+        self.client.post('/api/v1/meetups', json=meetup, headers=self.headers)
+        self.client.post('/api/v1/meetups', json=meetup2, headers=self.headers)
+
+        res = self.client.get('/api/v1/meetups/1')
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['status'], 200)
+        self.assertEqual(data['data'][0], 1)
+
+    def test_fetch_non_existent_meetup(self):
+        """ Test fetch a non existing meetup """
+        res = self.client.get('/api/v1/meetups/10')
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['status'], 404)
+        self.assertEqual(data['error'], 'Meetup not found')
