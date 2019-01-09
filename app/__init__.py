12a70dfe3e7803623b196dde4fb8f83cd0384704
Vincent Tirgei <tirgeic@gmail.com>
2019-01-09 14:47:55 +0300

    [Finishes #163039562] Registered questions Blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from instance.config import app_config
 from flask_jwt_extended import (JWTManager)
 from app.api.v1.views.user_view import v1 as users_blueprint_v1
 from app.api.v1.views.meetup_view import v1 as meetups_blueprint_v1
+from app.api.v1.views.question_view import v1 as questions_blueprint_v1
 
 def create_app(config_name):
     """ Function to initialize Flask app """
@@ -18,5 +19,6 @@ def create_app(config_name):
     # Register V1 Blueprints
     app.register_blueprint(users_blueprint_v1)
     app.register_blueprint(meetups_blueprint_v1)
+    app.register_blueprint(questions_blueprint_v1)
 
     return app
