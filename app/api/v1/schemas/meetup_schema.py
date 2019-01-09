14b2d5324cebe215ed30b59acf67765381c5711d
Vincent Tirgei <tirgeic@gmail.com>
2019-01-09 09:59:06 +0300

    [Feature #163039478] Added schema for meetup object

---
+++ app/api/v1/schemas/meetup_schema.py
@@ -0,0 +1,13 @@
+from marshmallow import Schema, fields, post_dump
+from ..utils.validator import required
+from ..models.user_model import User
+
+class MeetupSchema(Schema):
+    """ Class to validate schema for Meetup object """
+
+    id = fields.Int(dump_only=True)
+    topic = fields.Str(required=True, validate=(required))
+    location = fields.Str(required=True, validate=(required))
+    happening_on = fields.Str(required=True, validate=(required))
+    tags = fields.List(required=False, fields.Str())
+    images = fields.List(required=False, fields.Str())
