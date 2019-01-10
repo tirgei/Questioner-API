e5b6fd2d1100a85f5b70f559e5e6f5072c236fdc
Vincent Tirgei <tirgeic@gmail.com>
2019-01-10 08:15:10 +0300

    [Start #163039965] Added upvote function

--- app/api/v1/models/question_model.py
+++ app/api/v1/models/question_model.py
@@ -14,3 +14,26 @@ class Question(object):
         data['votes'] = 0
         questions.append(data)
         return data
+
+    def exists(self, key, value):
+        """ Function to check if question with provided key, value exists """
+        found_questions = [question for question in questions if value == question[key]]
+        return len(found_questions) > 0 
+
+    def upvote(self, question_id):
+        """ Function to upvote question """
+        for question in questions:
+            if question['id'] == question_id:
+                question['votes'] = question['votes']+1
+
+            return question
+
+    def downvote(self, question_id):
+        """ Function to downvote question """
+        for question in questions:
+            if question['id'] == question_id:
+                question['votes'] = question['votes']-1
+
+            return question
+
+        
