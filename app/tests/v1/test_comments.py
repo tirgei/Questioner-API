dceb6d59ee0599914b5b8129e539c34840850b90
Vincent Tirgei <tirgeic@gmail.com>
2019-01-12 17:28:07 +0300

    [Start #163040719] Added tests for post comment

---
+++ app/tests/v1/test_comments.py
@@ -0,0 +1,76 @@
+from flask import json
+from .base_test import BaseTest
+from app.api.v1.models.comment_model import comments
+from app.api.v1.models.question_model import questions
+
+class TestComments(BaseTest):
+    """ Test class for comments endpoints """
+
+    def setUp(self):
+        """ Initialize variables to be used for tests """
+        super().setUp()
+
+        self.question = {
+            'title' : 'Intro to python',
+            'body' : 'Are we covering the basics?',
+            'meetup' : 2,
+            'created_by' : 4
+        }
+
+        self.comment = {
+            'body' : 'Should include tests in the agenda too'
+        }
+
+        self.comment_empty = {}
+
+        self.headers = {'Content-Type': 'application/json'}
+
+    def tearDown(self):
+        """ Destroy initialized variables """
+        comments.clear()
+        questions.clear()
+        super().tearDown()
+
+    def test_post_comment_question_not_posted(self):
+        """ Test post comment to a question that hasn't been posted """
+        res = self.client.post('/api/v1/questions/3/comments')
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['status'], 404)
+        self.assertEqual(data['message'], 'Question not found')
+
+    def test_post_comment_question_no_data(self):
+        """ Test post comment without question data """
+        # Post question
+        self.client.post('/api/v1/questions', json=self.question, headers=self.headers)
+
+        res = self.client.post('/api/v1/questions/1/comments')
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['status'], 400)
+        self.assertEqual(data['message'], 'No data provided')
+
+    def test_post_comment_question_empty_data(self):
+        """ Test post comment with empty data """
+        # Post question
+        self.client.post('/api/v1/questions', json=self.question, headers=self.headers)
+
+        res = self.client.post('/api/v1/questions/1/comments', json=json.dumps(self.comment_empty), headers=self.headers)
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['status'], 400)
+        self.assertEqual(data['message'], 'Invalid data. Please fill all required fields')
+
+    def test_post_comment(self):
+        """ Test post comment successfully """
+        self.client.post('/api/v1/questions', json=self.question, headers=self.headers)
+
+        res = self.client.post('/api/v1/questions/1/comments', json=self.comment, headers=self.headers)
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 201)
+        self.assertEqual(data['status'], 201)
+        self.assertEqual(data['message'], 'Comment posted successfully')
